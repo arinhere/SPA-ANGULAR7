76bd69fe3106b5df97ae66bdef47ffa1de9082b9
arinhere <arinhere@gmail.com>
2019-03-28 12:32:19 +0530

    Change to AWS@28.03.2019

--- src/app/core/services/products.service.ts
+++ src/app/core/services/products.service.ts
@@ -5,18 +5,21 @@ import { Store } from '@ngrx/store';
 
 @Injectable()
 export class ProductService{
+    apiEP="http://ec2-3-83-188-115.compute-1.amazonaws.com:8080/api/";
+    //apiEP="http://localhost:3000/";
+
     constructor(
         private _http: HttpClient, 
         private _store: Store<{products: fromApp.State}>){}
 
     addProduct(body){
-        var url="http://localhost:3000/api/products/add";
+        var url=this.apiEP + "products/add";
         return this._http.post(url,body);
     }
 
     listProduct(){
         //this._store.dispatch({type: 'LOAD_PRODUCTS'});
-        var url="http://localhost:3000/api/products";
+        var url=this.apiEP + "products";
         return this._http.get(url);
     }
 }
--- src/app/core/services/test.services.ts
+++ src/app/core/services/test.services.ts
@@ -3,26 +3,29 @@ import { HttpClient } from '@angular/common/http';
 
 @Injectable()
 export class TestService{
+    apiEP="http://ec2-3-83-188-115.compute-1.amazonaws.com:8080/api/";
+    //apiEP="http://localhost:3000/";
 
     constructor(private _http: HttpClient){}
 
     getData(){
-        var url="http://localhost:3000/api/user/getData";
+        var url=this.apiEP + "user/getData";
         return this._http.get(url);
     } 
     
     signUp(body){
-        var url="http://localhost:3000/api/user/signup";        
+        var url=this.apiEP + "user/signup";  
+        console.log(url)      ;
         return this._http.post(url, body);
     }
 
     login(body){
-        var url="http://localhost:3000/api/user/login";        
+        var url=this.apiEP +  "user/login";        
         return this._http.post(url, body);
     }
 
     deleteData(name){
-        var url="http://localhost:3000/api/user/deleteData/Arin";
+        var url=this.apiEP + "user/deleteData/Arin";
         return this._http.delete(url);
     }
 }
